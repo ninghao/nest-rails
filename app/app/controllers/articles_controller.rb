b209485423775249562e1a79e5177d8b00c6cd35
wanghao <117663444@qq.com>
2016-09-23 16:40:03 +0800

    show：显示资源

--- app/app/controllers/articles_controller.rb
+++ app/app/controllers/articles_controller.rb
@@ -8,6 +8,10 @@ class ArticlesController < ApplicationController
     redirect_to @article
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
   private
     def article_params
       params.require(:article).permit(:title, :body)
