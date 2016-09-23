2f7aabbcbc9113efc5fbf3de7acf84910d2ce6a6
wanghao <117663444@qq.com>
2016-09-23 16:32:15 +0800

    save：保存资源

--- app/app/controllers/articles_controller.rb
+++ app/app/controllers/articles_controller.rb
@@ -3,6 +3,13 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to @article
   end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
