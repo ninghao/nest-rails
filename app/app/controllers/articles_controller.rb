3c23b945c85324ffffc9fa5b86f5565d607d4d7a
wanghao <117663444@qq.com>
2016-09-23 17:07:33 +0800

    验证表单输入

--- app/app/controllers/articles_controller.rb
+++ app/app/controllers/articles_controller.rb
@@ -4,8 +4,11 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render "new"
+    end
   end
 
   def show
