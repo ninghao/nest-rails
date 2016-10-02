105b81671bf1352f17e6dde8a4f0c9422e2ba09e
wanghao <117663444@qq.com>
2016-10-02 22:24:15 +0800

    Polymorphic：多态关联

--- app/app/models/post.rb
+++ app/app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :tags
+  has_many :comments, as: :commentable
 end
--- app/db/schema.rb
+++ app/db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161002134642) do
+ActiveRecord::Schema.define(version: 20161002142026) do
+
+  create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.text     "content",          limit: 65535
+    t.string   "commentable_type"
+    t.integer  "commentable_id"
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id", using: :btree
+  end
+
+  create_table "photos", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title"
