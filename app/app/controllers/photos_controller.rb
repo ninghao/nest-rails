dc4806e42c79d7e2b877b4e775f5a6c0dff6457d
wanghao <117663444@qq.com>
2016-10-13 12:46:42 +0800

    redirect_to

--- app/app/controllers/photos_controller.rb
+++ app/app/controllers/photos_controller.rb
@@ -1,8 +1,5 @@
 class PhotosController < ApplicationController
   def index
-    # render template: 'index'
-    # render plain: 'hello'
-    # render html: '<h1>photos</h1>'.html_safe
-    # render json: { "title": "photos" }
+    # redirect_to '/popular', status: 301
   end
 end
